0ee8dacc691dd1cae7aa9cb8de26c956b66d68f9
ashikcse20 <ashikcse20@gmail.com>
2019-01-22 14:56:35 +0600

    Commit

---
+++ CSharpConsolePracticePad/Delegate/Delegate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsolePracticePad.Delegate
+{
+    //// A simple delegate example.
+    //using System;
+    //// Declare a delegate type.
+    public delegate string StrMod(string str);
+    class DelegateTest
+    {
+        // Replaces spaces with hyphens.
+      internal  static string ReplaceSpaces(string s)
+        {
+            Console.WriteLine("Replacing spaces with hyphens.");
+            return s.Replace(' ', '-');
+        }
+
+        public string ReplaceSpacesInsTanceMethod(string str)
+        {
+            Console.WriteLine("Replacing spaces with Blank.");
+            return str.Replace(" ", ""); 
+        }
+
+        // Remove spaces.
+       internal static string RemoveSpaces(string s)
+        {
+            string temp = "";
+            int i;
+            Console.WriteLine("Removing spaces. sdsd");
+            for (i = 0; i < s.Length; i++)
+                if (s[i] != ' ') temp += s[i];
+            return temp;
+        }
+        // Reverse a string.
+       public static string Reverse(string s)
+        {
+            string temp = "";
+            int i, j;
+            Console.WriteLine("Reversing string.");
+            for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+                temp += s[i];
+            return temp;
+        }
+     /*   static void Main()
+        {
+            // Construct a delegate.
+            StrMod strOp = new StrMod(ReplaceSpaces);
+            string str;
+           
+        // Call methods through the delegate.
+ str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+            strOp = new StrMod(RemoveSpaces);
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+            strOp = new StrMod(Reverse);
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+        }*/
+    }
+
+
+}
--- CSharpConsolePracticePad/Program.cs
+++ CSharpConsolePracticePad/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpConsolePracticePad.Delegate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,76 @@ using System.Threading.Tasks;
 
 namespace CSharpConsolePracticePad
 {
-    class Program
+
+    //public  delegate string StrMod(string str);
+    public class Program
     {
         static void Main(string[] args)
         {
+            Program pOb = new Program();
+            //  pOb.DelegatePractice1();
+            pOb.DelegatePractice1MethodGroupConversion();
+
         }
+
+        public void DelegatePractice1()
+        {
+            // Construct a delegate.
+            StrMod strOp = new StrMod(DelegateTest.ReplaceSpaces);
+            string str;
+
+            // Call methods through the delegate.
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+
+            strOp = new StrMod(DelegateTest.RemoveSpaces);
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+            strOp = new StrMod(DelegateTest.Reverse);
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+
+            // Calling Intance Class 
+            Console.WriteLine("Calling Intance Class");
+            DelegateTest DlgOb = new DelegateTest();
+            strOp = new StrMod(DlgOb.ReplaceSpacesInsTanceMethod);
+            str = strOp("My name is Muhammad Adshikuzzaman");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.CursorSize = 10;
+            Console.WriteLine("My space replaced name is : " + str);
+            Console.WriteLine();
+
+        }
+        public void DelegatePractice1MethodGroupConversion()
+        {
+            // Construct a delegate using method group conversion.
+            StrMod strOp = DelegateTest.ReplaceSpaces; // use method group conversion
+            string str;
+            // Call methods through the delegate.
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+            strOp = DelegateTest.RemoveSpaces; // use method group conversion
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+            strOp = DelegateTest.Reverse; // use method group conversion
+            str = strOp("This is a test.");
+            Console.WriteLine("Resulting string: " + str);
+            // Calling Intance Class 
+            Console.WriteLine("Calling Intance Class");
+            DelegateTest DlgOb = new DelegateTest();
+            strOp = DlgOb.ReplaceSpacesInsTanceMethod;
+            str = strOp("My name is Muhammad Adshikuzzaman");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.CursorSize = 10;
+            Console.WriteLine("My space replaced name is : " + str);
+            Console.WriteLine();
+        } 
+
     }
 }
